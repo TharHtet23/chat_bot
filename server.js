9e74db4365c0ccceb792c4802813d54e875543d7
TharHtet236401 <tharhteta3@gmail.com>
2024-09-06 17:57:19 +0700

    implement the webapp feature

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Welcome to the bot! How can I help you today?");
-});
-
-bot.onText(/\/help/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Sure, I'd be happy to help! What do you need?");
+    bot.sendMessage(chatId, 'Click to open the web app:', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Open Web App', web_app: { url: 'https://invitecodebackend-production.up.railway.app' } }]
+        ]
+      }
+  });
 });
